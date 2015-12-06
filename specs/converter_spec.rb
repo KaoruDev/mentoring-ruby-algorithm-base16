4b9c0857f498e93dfeee72e363878f9c220ae3cc
Kaoru Kohashigawa <dev@kaoruk.com>
2015-12-05 17:46:46 -0800

    add specs for converting from 16 back to 10

--- specs/converter_spec.rb
+++ specs/converter_spec.rb
@@ -27,5 +27,32 @@ RSpec.describe Converter do
 
     end
   end
+
+  describe "#from_base16" do
+    let(:converter) { Converter.new }
+
+    context "when passed" do
+      it "20 will return 17" do
+        expect(converter.from_base16("A")).to eq(10)
+      end
+
+      it "14 will return 20" do
+        expect(converter.from_base16("14")).to eq(20)
+      end
+
+      it "255 will return FF" do
+        expect(converter.from_base16("FF")).to eq(255)
+      end
+
+      it "1FE will return 510" do
+        expect(converter.from_base16("1FE")).to eq(510)
+      end
+
+      it "49960015 will return 1234567189" do
+        expect(converter.from_base16("49960015")).to eq(1234567189)
+      end
+
+    end
+  end
 end
 
